58c4ec87cabd32cbc7eed8f6b062eb43af67b728
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-09-04 20:39:18 +0200

    Adding MailHog script

---
+++ bin/mailhog.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# MailHog
+echo "Installing MailHog"
+apt-get install golang-go -y
+mkdir /run/gocode
+echo "export GOPATH=/run/gocode" >> ~/.profile
+source ~/.profile
+go get github.com/mailhog/MailHog
+go get github.com/mailhog/mhsendmail
+cp /run/gocode/bin/MailHog /usr/local/bin/mailhog
+cp /run/gocode/bin/mhsendmail /usr/local/bin/mhsendmail
+# Configure Sendmail for PHP
+echo "sendmail_path=/usr/local/bin/mhsendmail" >> /etc/php/8.1/apache2/conf.d/user.ini
+service apache2 restart
+# Configure MailHog to start as a system service
+IP="$(hostname -I | cut -f1 -d' ')"
+touch /etc/systemd/system/mailhog.service
+echo "[Unit]" >> /etc/systemd/system/mailhog.service
+echo "Description=MailHog service" >> /etc/systemd/system/mailhog.service
+echo "[Service]" >> /etc/systemd/system/mailhog.service
+echo "ExecStart=/usr/local/bin/mailhog -api-bind-addr $IP:8025 -ui-bind-addr $IP:8025 -smtp-bind-addr 127.0.0.1:1025" >> /etc/systemd/system/mailhog.service
+echo "[Install]" >> /etc/systemd/system/mailhog.service
+echo "WantedBy=multi-user.target" >> /etc/systemd/system/mailhog.service
+systemctl enable mailhog
+service mailhog start
--- macos/install.sh
+++ macos/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-multipass launch --name wp-local-env --cloud-init ./cloud-init-for-wp-local-env.yaml
+multipass launch --name wp-local-env --mem 2G --disk 10G --cpus 2 --cloud-init ../config/cloud-init-for-wp-local-env.yaml
 
 mkdir -p ~/wp-local-env
 
@@ -9,3 +9,7 @@ multipass mount ~/wp-local-env wp-local-env:/home/ubuntu/wp-local-env
 cd ~/wp-local-env
 
 mkdir -p sites ssh-keys
+
+# copy mailhog script to the server, and chmod +x it
+# run the mailhog script to install mailhog
+# multipass exec wp-local-env -- sudo -u root ./mailhog.sh
