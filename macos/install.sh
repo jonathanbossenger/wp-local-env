497aace61466d74babf5fe8a6da4d02d855b0f3c
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-09-05 23:40:36 +0200

    Updating install

--- macos/install.sh
+++ macos/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Launch the new instance
-multipass launch --name wp-local-env --mem 2G --disk 10G --cpus 2 --cloud-init ../config/cloud-init-for-wp-local-env.yaml
+wget https://github.com/jonathanbossenger/wp-local-env/blob/trunk/config/cloud-init-for-wp-local-env.yaml
+multipass launch --name wp-local-env --mem 2G --disk 10G --cpus 2 --cloud-init cloud-init-for-wp-local-env.yaml
+rm cloud-init-for-wp-local-env.yaml
 
 # Install MailHog
 multipass exec wp-local-env -- wget https://github.com/jonathanbossenger/wp-local-env/blob/trunk/bin/mailhog.sh
@@ -19,15 +21,16 @@ multipass exec wp-local-env -- wget https://github.com/jonathanbossenger/wp-loca
 multipass exec wp-local-env -- wget https://github.com/jonathanbossenger/wp-local-env/blob/trunk/macos/scripts/multipass/sitedrop.sh
 multipass exec wp-local-env -- chmod +x sitesetup.sh
 multipass exec wp-local-env -- chmod +x sitedrop.sh
-multipass exec wp-local-env -- sudo -u cp sitesetup.sh /usr/local/bin/sitesetup
-multipass exec wp-local-env -- sudo -u cp sitedrop.sh /usr/local/bin/sitedrop
+multipass exec wp-local-env -- sudo -u mv sitesetup.sh /usr/local/bin/sitesetup
+multipass exec wp-local-env -- sudo -u mv sitedrop.sh /usr/local/bin/sitedrop
 
 # Install OS Scripts
-chmod +x ./scripts/os/sitesetup.sh
-chmod +x ./scripts/os/sitedrop.sh
-sudo cp ./scripts/os/sitesetup.sh /usr/local/bin/sitesetup
-sudo cp ./scripts/os/sitedrop.sh /usr/local/bin/sitedrop
-
+wget https://github.com/jonathanbossenger/wp-local-env/blob/trunk/macos/scripts/os/sitesetup.sh
+wget https://github.com/jonathanbossenger/wp-local-env/blob/trunk/macos/scripts/os/sitedrop.sh
+chmod +x sitesetup.sh
+chmod +x sitedrop.sh
+sudo mv sitesetup.sh /usr/local/bin/sitesetup
+sudo mv sitedrop.sh /usr/local/bin/sitedrop
 
 # Setup /etc/hosts record for wp-local-env.test
 INSTANCE_DATA=$( multipass info --format json wp-local-env )
