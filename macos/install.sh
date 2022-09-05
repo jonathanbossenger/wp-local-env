2cf638dd5e924dde03e821cc6c75a0b16b98530c
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-09-05 23:50:04 +0200

    URL updates

--- macos/install.sh
+++ macos/install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# install
+# curl -o- curl -o- https://raw.githubusercontent.com/jonathanbossenger/wp-local-env/e476fb0dc5103a72feab6ea67cfb2a1c256660ba/macos/install.sh > install.sh
+
 # Launch the new instance
-wget https://github.com/jonathanbossenger/wp-local-env/blob/trunk/config/cloud-init-for-wp-local-env.yaml
+curl -o- https://raw.githubusercontent.com/jonathanbossenger/wp-local-env/e476fb0dc5103a72feab6ea67cfb2a1c256660ba/config/cloud-init-for-wp-local-env.yaml > cloud-init-for-wp-local-env.yaml
 multipass launch --name wp-local-env --mem 2G --disk 10G --cpus 2 --cloud-init cloud-init-for-wp-local-env.yaml
 rm cloud-init-for-wp-local-env.yaml
 
 # Install MailHog
-multipass exec wp-local-env -- wget https://github.com/jonathanbossenger/wp-local-env/blob/trunk/bin/mailhog.sh
+multipass exec wp-local-env -- wget https://raw.githubusercontent.com/jonathanbossenger/wp-local-env/e476fb0dc5103a72feab6ea67cfb2a1c256660ba/bin/mailhog.sh > mailhog.sh
 multipass exec wp-local-env -- chmod +x mailhog.sh
 multipass exec wp-local-env -- sudo -u root ./mailhog.sh
 
@@ -17,16 +20,16 @@ cd ~/wp-local-env
 mkdir -p sites ssl-certs
 
 # Install Server Scripts
-multipass exec wp-local-env -- wget https://github.com/jonathanbossenger/wp-local-env/blob/trunk/macos/scripts/multipass/sitesetup.sh
-multipass exec wp-local-env -- wget https://github.com/jonathanbossenger/wp-local-env/blob/trunk/macos/scripts/multipass/sitedrop.sh
+multipass exec wp-local-env -- wget https://raw.githubusercontent.com/jonathanbossenger/wp-local-env/e476fb0dc5103a72feab6ea67cfb2a1c256660ba/macos/scripts/multipass/sitesetup.sh > sitesetup.sh
+multipass exec wp-local-env -- wget https://raw.githubusercontent.com/jonathanbossenger/wp-local-env/e476fb0dc5103a72feab6ea67cfb2a1c256660ba/macos/scripts/multipass/sitedrop.sh > sitedrop.sh
 multipass exec wp-local-env -- chmod +x sitesetup.sh
 multipass exec wp-local-env -- chmod +x sitedrop.sh
 multipass exec wp-local-env -- sudo -u mv sitesetup.sh /usr/local/bin/sitesetup
 multipass exec wp-local-env -- sudo -u mv sitedrop.sh /usr/local/bin/sitedrop
 
 # Install OS Scripts
-wget https://github.com/jonathanbossenger/wp-local-env/blob/trunk/macos/scripts/os/sitesetup.sh
-wget https://github.com/jonathanbossenger/wp-local-env/blob/trunk/macos/scripts/os/sitedrop.sh
+curl -o- https://raw.githubusercontent.com/jonathanbossenger/wp-local-env/e476fb0dc5103a72feab6ea67cfb2a1c256660ba/macos/scripts/os/sitesetup.sh > sitesetup.sh
+curl -o- https://raw.githubusercontent.com/jonathanbossenger/wp-local-env/e476fb0dc5103a72feab6ea67cfb2a1c256660ba/macos/scripts/os/sitedrop.sh > sitedrop.sh
 chmod +x sitesetup.sh
 chmod +x sitedrop.sh
 sudo mv sitesetup.sh /usr/local/bin/sitesetup
