372769e34e011f1ae72858f0443d0f014e9fd903
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-09-10 13:31:24 +0200

    Removing hosts record

--- macos/install.sh
+++ macos/install.sh
@@ -42,17 +42,17 @@ sudo mv sitesetup.sh /usr/local/bin/sitesetup
 sudo mv sitedrop.sh /usr/local/bin/sitedrop
 
 # Setup /etc/hosts record for wp-local-env.test
-echo "Setting up hosts record..."
-INSTANCE_DATA=$( multipass info --format json wp-local-env )
-read -r -d '' JXA <<EOF
-function run() {
-	var info = JSON.parse(\`$INSTANCE_DATA\`);
-	return info.info["wp-local-env"].ipv4;
-}
-EOF
-INSTANCE_IP=$( osascript -l 'JavaScript' <<< "${JXA}" )
+# echo "Setting up hosts record..."
+# INSTANCE_DATA=$( multipass info --format json wp-local-env )
+# read -r -d '' JXA <<EOF
+# function run() {
+# 	var info = JSON.parse(\`$INSTANCE_DATA\`);
+# 	return info.info["wp-local-env"].ipv4;
+# }
+# EOF
+# INSTANCE_IP=$( osascript -l 'JavaScript' <<< "${JXA}" )
 # sudo echo "$INSTANCE_IP    wp-local-env.test" >> /etc/hosts
-echo "Enter your sudo password to enable the hosts record..."
-echo -n password | sudo -S echo "$INSTANCE_IP    wp-local-env.test" >> /etc/hosts
+# echo "Enter your sudo password to enable the hosts record..."
+# echo -n password | sudo -S echo "$INSTANCE_IP    wp-local-env.test" >> /etc/hosts
 
 echo "Done!"
