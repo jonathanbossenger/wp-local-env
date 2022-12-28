fdd9c319da11b1331e8e0c140227dced96c92248
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-12-28 21:29:32 +0200

    Adding PHP 8.2 support
    As well as 8.1 and 8.0

--- linux/scripts/multipass/sitesetup.sh
+++ linux/scripts/multipass/sitesetup.sh
@@ -47,6 +47,40 @@ SSL_VIRTUAL_HOST="<IfModule mod_ssl.c>
     </VirtualHost>
 </IfModule>"
 
+## PHP 8.1 virtual host
+if [ "$PHP_VERSION" == '8.1' ]; then
+    SSL_VIRTUAL_HOST="<IfModule mod_ssl.c>
+        <VirtualHost _default_:443>
+            ServerName $SITE_NAME.test
+            ServerAdmin webmaster@$SITE_NAME.test
+            DocumentRoot $SITES_DIRECTORY/$SITE_NAME
+            <Directory \"$SITES_DIRECTORY/$SITE_NAME\">
+                #Require local
+                Order allow,deny
+                Allow from all
+                AllowOverride all
+                # New directive needed in Apache 2.4.3:
+                Require all granted
+            </Directory>
+            ErrorLog \${APACHE_LOG_DIR}/$SITE_NAME-error.log
+            CustomLog \${APACHE_LOG_DIR}/$SITE_NAME-access.log combined
+            SSLEngine on
+            SSLCertificateFile  $SSL_CERTS_DIRECTORY/$SITE_NAME.test.pem
+            SSLCertificateKeyFile $SSL_CERTS_DIRECTORY/$SITE_NAME.test-key.pem
+            <FilesMatch \.php$>
+                # From the Apache version 2.4.10 and above, use the SetHandler to run PHP as a fastCGI process s>
+                SetHandler \"proxy:unix:/run/php/php8.1-fpm.sock|fcgi://localhost\"
+            </FilesMatch>
+            <FilesMatch \"\.(cgi|shtml|phtml|php)\$\">
+                    SSLOptions +StdEnvVars
+            </FilesMatch>
+            <Directory /usr/lib/cgi-bin>
+                    SSLOptions +StdEnvVars
+            </Directory>
+        </VirtualHost>
+    </IfModule>"
+fi
+
 ## PHP 8.0 virtual host
 if [ "$PHP_VERSION" == '8.0' ]; then
     SSL_VIRTUAL_HOST="<IfModule mod_ssl.c>
